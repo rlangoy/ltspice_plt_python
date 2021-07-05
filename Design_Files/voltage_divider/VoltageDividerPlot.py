02f8b85cdecf7a0377fc3d8dd34c8f75896abdbf
Rune Langøy <rune.langoy@gmail.com>
2021-07-05 10:12:32 +0200

    Added Example

---
+++ Design_Files/voltage_divider/VoltageDividerPlot.py
@@ -0,0 +1,37 @@
+import ltspice
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+#Input Plot file
+filepath = r'VoltageDivider.raw'
+l = ltspice.Ltspice(filepath)
+l.parse() # Data loading sequence. It may take few minutes for huge file.
+print("Filen "+ filepath)
+print("Inneholder følgende variabler : ")
+for name in l.variables:
+    print(name)
+
+#For å legge inn matematiske utrykk inn i plotet se:
+# W  riting mathematical expressions in Matplotlib. https://matplotlib.org/stable/tutorials/text/mathtext.html
+ohm=r'$\Omega$'
+
+#Data Som skal plottes på X/Y-Aksen
+xAxisValues = l.get_data('var_r1')
+yAxisValues = l.get_data('v(vout)')
+
+#Plot X/Y aksen og ha med lavelen Vout
+plt.plot(xAxisValues, yAxisValues,label='Vout')
+
+#Sett akse navn
+plt.xlabel('Motstandsverdi R1 [%s]'%ohm)
+plt.ylabel('Vout [V]')
+
+#Sett navn på plottet
+plt.title("Utgans spenningen Vout")
+
+#Vis labelen 'Vout'
+plt.legend()
+
+#Vis plot dialogboksen
+plt.show()
